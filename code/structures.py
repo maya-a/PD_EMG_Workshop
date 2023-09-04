3501d129be61ad6d19f077dc24ef22510b434513
maya-a <mayaad0@gmail.com>
2023-09-04 09:31:10 +0300

    Add files via upload

--- code/structures.py
+++ code/structures.py
@@ -1,1 +1,16 @@
+control = ['AA4', 'AD5', 'AD7', 'BP5', 'BZ4', 
+           'DC9', 'KS6', 'LK4', 'LS8', 'MG1',
+           'MK39', 'ML6', 'MU6', 'OS8', 'RA64', 
+           'RS2', 'SA50', 'TBN8', 'VB10', 'YL02']
+parkinsons = ['AC4', 'AH67', 'AP7', 'AS98', 'ED8',
+              'HR67', 'MM4', 'NF1', 'RK4', 'RT1', 
+              'SA7', 'SB2', 'TY0', 'VH5', 'VP2',
+              'YAA0', 'YG2', 'YG8', 'YH8' , 'YP8', 
+              'YZ6', 'YZ64', 'ZZ0']
+subset = ['E226', 'E228', 'E230', 'E234', 
+          'E235', 'E238', 'E239', 'E240', 
+          'E241', 'E242', 'E243', 'E244', 
+          'E245', 'E248','252' 'E255']
 
+groups = ["control", "parkinsons"]
+subjects_by_group = [control, parkinsons]
